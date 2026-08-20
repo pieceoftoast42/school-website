b262e5f6d3e7371fdd1288c8bad0ee6d41be3252
pieceoftoast42 <148928746+pieceoftoast42@users.noreply.github.com>
2026-08-19 18:36:34 -0700

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hcxfamtbswhczobfhjjf.supabase.co" ;
+const supabaseKey = "sb_publishable_jrQ0pDat8B0kor7R2upO4g_mUL06ziy";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
